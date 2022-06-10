d3b7702ccce5646309b9cafb996448162be8a229
SiegeTank-90 <ccline140@yahoo.com>
2022-06-10 09:28:42 -0500

    Morning Updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Filter from "./components/Filter";
 import FlagGallery from "./components/FlagGallery";
 
+function makeId(length) {
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 function App() {
+  useEffect(() => {
+    async function getRandomFlags() {
+      try {
+        const response = await axios.get(
+          {"https://restcountries.com/v3.1/alpha?codes=" +
+            makeId(3) +
+            "," +
+            makeId(3) +
+            "," +
+            makeId(3) +
+            "," +
+            makeId(3) +
+            "," +
+            makeId(3) +
+            "," +
+            makeId(3) +
+            "," +
+            makeId(3) +
+            "," +
+            makeId(3)}
+        );
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getRandomFlags();
+  }, []);
 
- const [Theme, setTheme] = useState("Light")  
+  const [Theme, setTheme] = useState("Dark");
+  const [SearchParm, setSetParm] = useState("Search for a Country...");
 
+  function FilterByRegion() {
+    console.log("Filter");
+    return null;
+  }
 
   return (
     <div className={"App " + Theme}>
-      <div className="Background" >
-        <Header setTheme={setTheme} />
-        <div className="Element">
-          <Search />
-          <Filter />
+      <div className="Background">
+        <Header Theme={Theme} setTheme={setTheme} />
+        <div className="Flex">
+          <Search value={SearchParm} setValue={setSetParm} />
+          <Filter Filter={FilterByRegion} />
         </div>
-        <FlagGallery />
+        <FlagGallery Data="" />
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
-function Header() {
+function Header(props) {
+  if (props.Theme === "Dark") {
+    var DisplayName = "Light";
+    DisplayIcon = <FontAwesomeIcon icon={faSun} />;
+  } else {
+    DisplayName = "Dark";
+    var DisplayIcon = <FontAwesomeIcon icon={faMoon} />;
+  }
 
-    return (
-        <h1>Hello Header</h1>
-    )
+  function handleClick(e) {
+    if (props.Theme === "Dark") {
+      props.setTheme("Light");
+    } else props.setTheme("Dark")
+  }
 
+  return (
+    <header className="Element Flex Header">
+      <h1 className="Title">Where in the World?</h1>
+      <div onClick={handleClick} className="ThemeChangerButton">
+        {DisplayIcon}
+        <p className="ThemeName">{DisplayName} Mode</p>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
