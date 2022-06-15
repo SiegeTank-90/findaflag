e9cf246bd01c79fe3dd133ad8c6f2ae389f170a0
SiegeTank-90 <ccline140@yahoo.com>
2022-06-15 12:08:22 -0500

    1.0 Launch Test

---
+++ src/components/BorderButton.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function BorderButton(props) {
+  const [Name, setName] = useState("");
+
+  async function getCountry() {
+    try {
+      const response = await axios.get(
+        "https://restcountries.com/v3.1/alpha/" + [props.code]
+      );
+
+      setName(response.data[0].name.common);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getCountry();
+  },[]);
+
+  return (
+    
+    <Link reloadDocument className="Element BorderButton"  to={`/countries/${Name}`}>
+      <h3 className="BorderButtonContent">{Name}</h3>
+    </Link>
+    
+  );
+}
+
+export default BorderButton;
--- src/pages/DetailedInformation.js
+++ src/pages/DetailedInformation.js
@@ -2,6 +2,9 @@ import React, { Suspense, useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import { faLockOpen } from "@fortawesome/free-solid-svg-icons";
+import BorderButton from "../components/BorderButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function DetailsPage() {
   const [Details, setDetails] = useState({});
@@ -10,7 +13,7 @@ function DetailsPage() {
   async function getCountry() {
     try {
       const response = await axios.get(
-        "https://restcountries.com/v3.1/name/" + name
+        "https://restcountries.com/v3.1/name/" + name + "?fullText=True"
       );
 
       setDetails(response.data[0]);
@@ -25,7 +28,6 @@ function DetailsPage() {
     getCountry();
   }, []);
 
-  console.log(Details);
   if (isLoading === true) {
     return <h1 className="ThemeTextColor"> Loading . . .</h1>;
   }
@@ -33,56 +35,79 @@ function DetailsPage() {
   //Breaking Down JSON DATA
   let NativeNameArray = Object.values(Details.name.nativeName);
   let NativeName = NativeNameArray[NativeNameArray.length - 1].common;
-  let LanguagesArray = Object.values(Details.languages)
+  let LanguagesArray = Object.values(Details.languages);
   let Languages = " ";
-  console.log(LanguagesArray.length)
-  for (let i = 0 ; i < LanguagesArray.length; i++) {
-      Languages += LanguagesArray[i]
-      if ( i != LanguagesArray.length-1) {
-          Languages += ", "
-      }
+  for (let i = 0; i < LanguagesArray.length; i++) {
+    Languages += LanguagesArray[i];
+    if (i != LanguagesArray.length - 1) {
+      Languages += ", ";
+    }
   }
-
+  let CurrenciesArray = Object.values(Details.currencies);
+  let Currencies = " ";
+  for (let i = 0; i < CurrenciesArray.length; i++) {
+    Currencies += CurrenciesArray[i].name;
+    if (i != CurrenciesArray.length - 1) {
+      Currencies += ", ";
+    }
+  }
+  let BorderButtonArray = [];
+  if (Details.borders != null) {
+  let BorderingArray = Object.values(Details.borders);
+  for (let i = 0; i < BorderingArray.length; i++) {
+      BorderButtonArray.push(<BorderButton code={BorderingArray[i]} />);
+    }
+  }
 
   //Breaking Down JSON DATA
 
   return (
-    <>
+    <div className="DetailsContainer">
       <Link to="/" className="BackButton Element">
-        Back
+        <FontAwesomeIcon icon={faArrowLeft} /> <p className="Back">Back</p>
       </Link>
-      <img className="DetailsFlag" src={Details.flags.png}></img>
-      <div className="ThemeTextColor">
+      <img className="DetailsFlag Element" src={Details.flags.png}></img>
+      <div className="ThemeTextColor DetialsTextContainer">
         <h1 className="DetailsName">{Details.name.common}</h1>
-        <h3 className="Detailstitle">
-          Native Name: <span className="DetailsInfo">{NativeName}</span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Population:{" "}
-          <span className="DetailsInfo">
-            {new Intl.NumberFormat("en-us").format(Details.population)}
-          </span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Region: <span className="DetailsInfo">{Details.region}</span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Sub-Region: <span className="DetailsInfo">{Details.subregion}</span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Capitial: <span className="DetailsInfo">{Details.capital}</span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Top Level Domain: <span className="DetailsInfo">{Details.tld}</span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Currencies <span className="DetailsInfo">{Details.tld}</span>{" "}
-        </h3>
-        <h3 className="Detailstitle">
-          Languages: <span className="DetailsInfo">{Languages}</span>{" "}
-        </h3>
+        <div className="DetailsBlock">
+          <h3 className="Detailstitle">
+            Native Name : <span className="DetailsInfo">{NativeName}</span>{" "}
+          </h3>
+          <h3 className="Detailstitle">
+            Population :
+            <span className="DetailsInfo">
+              {" " + new Intl.NumberFormat("en-us").format(Details.population)}
+            </span>
+          </h3>
+          <h3 className="Detailstitle">
+            Region : <span className="DetailsInfo">{Details.region}</span>{" "}
+          </h3>
+          <h3 className="Detailstitle">
+            Sub-Region :{" "}
+            <span className="DetailsInfo">{Details.subregion}</span>{" "}
+          </h3>
+          <h3 className="Detailstitle">
+            Capitial : <span className="DetailsInfo">{Details.capital}</span>{" "}
+          </h3>
+        </div>
+        <div className="DetailsBlock">
+          <h3 className="Detailstitle">
+            Top Level Domain :{" "}
+            <span className="DetailsInfo">{Details.tld}</span>{" "}
+          </h3>
+          <h3 className="Detailstitle">
+            Currencies : <span className="DetailsInfo">{Currencies}</span>{" "}
+          </h3>
+          <h3 className="Detailstitle">
+            Languages : <span className="DetailsInfo">{Languages}</span>{" "}
+          </h3>
+        </div>
+        <div>
+          <h2>Border Countries</h2>
+          <div className="Flex BorderButtonContainer ">{BorderButtonArray}</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
